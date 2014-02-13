a9dc73a07aa371ce9a8f23b6ad227f742f950088
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-02-13 21:39:50 +0100

    CommentsController updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  expose(:comment)
+
+  def mark_as_not_abusive
+    comment.update_attribute(:abusive, false)
+    redirect_to comment.post
+  end
+
+  def vote_up
+    comment.votes.find_or_create_by(value: 1, user: current_user)
+    redirect_to comment.post
+  end
 end
