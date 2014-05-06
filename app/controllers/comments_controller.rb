68f8effe11c4060671532cf529fea613382a66bf
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-05-06 16:58:38 +0200

    voting system and validation created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,6 +28,8 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    if params[:comment]
+      params.require(:comment).permit(:body)
+    end
   end
 end
